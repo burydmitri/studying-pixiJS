5a1739d37f3c782d05634f343c7f4739acf3e75b
mityaaa <burydima28122003@gmail.com>
2024-06-26 13:39:59 +0300

    start: create vue@latest
    Create project and make some preparations

---
+++ trader-character/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div ref="app" class="pixi-app">
+
+  </div>
+</template>
+
+<script>
+import { Application, Assets } from 'pixi.js';
+
+const pixiApp = new Application();
+
+export default {
+  methods: {
+    async initPixiApp() {
+      await pixiApp.init({ background: '#222222'});
+      this.$refs.app.appendChild(pixiApp.canvas)
+    }
+  },
+  mounted() {
+    this.initPixiApp()
+  }
+}
+</script>
+
+<style scoped>
+.pixi-app {
+  height: 600px;
+}
+</style>
---
+++ trader-character/vite.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { compilerOptions, transformAssetUrls } from 'vue3-pixi'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      compilerOptions,
+      transformAssetUrls,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
