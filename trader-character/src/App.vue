de6d6c01c7469ce4412e076efb3f16d8b05386d9
mityaaa <burydima28122003@gmail.com>
2024-06-27 12:27:01 +0300

    feature: create api for bombs and display red bomb

--- trader-character/src/App.vue
+++ trader-character/src/App.vue
@@ -6,25 +6,28 @@
 
 <script>
 import { Application, Spritesheet, Texture, AnimatedSprite, Assets } from 'pixi.js';
-import { assetsFetch, hero } from './api';
+import { createBomb } from './api';
 
 const pixiApp = new Application();
 
 export default {
   methods: {
-    async initPixiApp() {
-      await this.createCanvas()
-      await assetsFetch()
-      const anim = await hero()
-      anim.animationSpeed = 0.025
-      anim.x = pixiApp.screen.width / 2;
-      anim.y = pixiApp.screen.height / 2;
-      anim.play()
-      pixiApp.stage.addChild(anim);
-    },
     async createCanvas() {
       await pixiApp.init({ background: '#222222' });
       this.$refs.app.appendChild(pixiApp.canvas)
+    },
+    async displayBomb(x, y, standSpeed, explSpeed) {
+      const bomb = await createBomb(x, y, standSpeed, explSpeed, () => {
+        bomb.expldAnim.play()
+        pixiApp.stage.addChild(bomb.expldAnim);
+      })
+
+      bomb.standAnim.play()
+      pixiApp.stage.addChild(bomb.standAnim);
+    },
+    async initPixiApp() {
+      await this.createCanvas()
+      await this.displayBomb(pixiApp.screen.width / 2, pixiApp.screen.height / 2, 0.15, 0.2) 
     },
   },
   mounted() {
--- trader-character/src/api.js
+++ trader-character/src/api.js
@@ -1,79 +1,70 @@
 import { Spritesheet, Texture, AnimatedSprite, Assets } from 'pixi.js';
 
-async function assetsFetch() {
-    await Assets.load('../src/assets/approval.png')
-}
-
-async function hero() {
+async function createAnimation(frames, w, h, image) {
     const data = {
-        frames: {
-            hero1: {
-                frame: { x: 0, y: 0, w: 128, h: 128 },
-                sourceSize: { w: 128, h: 128 },
-                spriteSourceSize: { x: 0, y: 0, w: 128, h: 128 }
-            },
-            hero2: {
-                frame: { x: 128, y: 0, w: 128, h: 128 },
-                sourceSize: { w: 128, h: 128 },
-                spriteSourceSize: { x: 0, y: 0, w: 128, h: 128 }
-            },
-            hero3: {
-                frame: { x: 256, y: 0, w: 128, h: 128 },
-                sourceSize: { w: 128, h: 128 },
-                spriteSourceSize: { x: 0, y: 0, w: 128, h: 128 }
-            },
-            hero4: {
-                frame: { x: 384, y: 0, w: 128, h: 128 },
-                sourceSize: { w: 128, h: 128 },
-                spriteSourceSize: { x: 0, y: 0, w: 128, h: 128 }
-            },
-            hero5: {
-                frame: { x: 512, y: 0, w: 128, h: 128 },
-                sourceSize: { w: 128, h: 128 },
-                spriteSourceSize: { x: 0, y: 0, w: 128, h: 128 }
-            },
-            hero6: {
-                frame: { x: 640, y: 0, w: 128, h: 128 },
-                sourceSize: { w: 128, h: 128 },
-                spriteSourceSize: { x: 0, y: 0, w: 128, h: 128 }
-            },
-            hero7: {
-                frame: { x: 768, y: 0, w: 128, h: 128 },
-                sourceSize: { w: 128, h: 128 },
-                spriteSourceSize: { x: 0, y: 0, w: 128, h: 128 }
-            },
-            hero8: {
-                frame: { x: 896, y: 0, w: 128, h: 128 },
-                sourceSize: { w: 128, h: 128 },
-                spriteSourceSize: { x: 0, y: 0, w: 128, h: 128 }
-            },
-        },
+        frames: {},
         meta: {
-            image: '../src/assets/approval.png',
-            size: { w: 1024, h: 128 },
-            scale: 0.25
+            image: image,
+            size: { w: w * frames, h: h },
+            scale: 1
         },
         animations: {
-            hero: ['hero1', 'hero2', 'hero3', 'hero4', 'hero5', 'hero6', 'hero7', 'hero8'] //array of frames by name
-        }
+            anim: []
+        },
     }
 
+    for (let i = 0; i < frames; i++) {
+        data.frames[`anim${i}`] = {
+            frame: { x: i * w, y: 0, w: w, h: h },
+            sourceSize: { w: w, h: h },
+            spriteSourceSize: { x: 0, y: 0, w: w, h: h }
+        }
+        data.animations.anim.push(`anim${i}`)
+    }
 
-    // Create the SpriteSheet from data and image
     const spritesheet = new Spritesheet(
         Texture.from(data.meta.image),
         data
     );
 
-    // Generate all the Textures asynchronously
     await spritesheet.parse();
 
-    // spritesheet is ready to use!
-    const anim = new AnimatedSprite(spritesheet.animations.hero);
+    const animation = new AnimatedSprite(spritesheet.animations.anim);
+    animation.anchor.set(0.5);
 
-    anim.anchor.set(0.5);
-
-    return anim
+    return animation
 }
 
-export { assetsFetch, hero }
+async function createBombAnimations(standFrames, explFrames, w, h, standImage, explImage) {
+    const expldAnim = await createAnimation(explFrames, w, h, explImage)
+    const standAnim = await createAnimation(standFrames, w, h, standImage)
+
+    return { standAnim, expldAnim }
+}
+
+async function createBomb(x, y, standSpeed, explSpeed, onClick) {
+
+    await Assets.load(['../src/assets/red.png', '../src/assets/expl-red.png'])
+    const bomb = await createBombAnimations(3, 11, 50, 50, '../src/assets/red.png', '../src/assets/expl-red.png')
+
+    bomb.standAnim.x = bomb.expldAnim.x = x
+    bomb.standAnim.y = bomb.expldAnim.y = y
+
+    bomb.expldAnim.animationSpeed = explSpeed
+    bomb.standAnim.animationSpeed = standSpeed
+
+    bomb.standAnim.interactive = true;
+    bomb.standAnim.on('click', () => { 
+        bomb.standAnim.destroy()
+        onClick()
+    })
+
+    bomb.expldAnim.loop = false
+    bomb.expldAnim.onComplete = () => { 
+        bomb.expldAnim.destroy()
+    }
+
+    return bomb
+}
+
+export { createBomb }
